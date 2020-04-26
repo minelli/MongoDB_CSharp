39888c427bba1040788f5da3cec811512269e8ae
Marcelo Minelli <mrminelli@live.com>
2020-04-26 13:46:41 -0400

    ** Français au dessous / English below  **
    Criando o projeto
    - Instalando driver de conexão ao MongoDB e configuração de acesso ao banco
    - Criação da classe model de Usuario
    - Implementação do código de inserção de registro (único e muitos)
    
    ** Français
    Création du projet
    - Instalation du driver de connexion au MongoDB et configuration du accès à la base de données
    - Création de la classe modèle User
    - Création du code à insérer des enregistrements (un ou plusieurs)
    
    ** English
    Creating the project
    - Installing the connection driver to MongoDB and configuring the access to db
    - Creating the model User
    - Creating the code to insert registers (one and many)

---
+++ MongoDB_CSharp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB_CSharp.Models
+{
+    public class User
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+    }
+}
--- MongoDB_CSharp/Program.cs
+++ MongoDB_CSharp/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using MongoDB.Driver;
+
+using MongoDB_CSharp.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MongoDB_CSharp
 {
@@ -10,6 +10,56 @@ namespace MongoDB_CSharp
     {
         static void Main(string[] args)
         {
+            const string connectionString = "mongodb://localhost:27017";
+
+            try
+            {
+                var client = new MongoClient(connectionString);
+                var database = client.GetDatabase("loja");
+
+                var collection = database.GetCollection<User>("customers");
+
+                Console.WriteLine("Connect with the server!");
+
+                //Insert one register
+                var user = new User
+                {
+                    email = "user@email.com",
+                    name = "User",
+                    password = "12345"
+                };
+
+                collection.InsertOne(user);
+                Console.WriteLine("Insert one register into collection");
+
+                //Insert many registers
+                var userList = new List<User>
+                {
+                    new User
+                    {
+                        email = "user2@email.com",
+                        name = "User 2",
+                        password = "12345"
+                    },
+                    new User
+                    {
+                        email = "user3@email.com",
+                        name = "User 3",
+                        password = "12345"
+                    }
+                };
+
+                collection.InsertMany(userList);
+                Console.WriteLine("Insert a list of registers into collection");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine($"Error: {e.Message}");
+                Console.ReadLine();
+            }
         }
     }
 }
